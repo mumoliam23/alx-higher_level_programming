4e32c768cf86b8c8fd29bb70c3a086bd02120d19
mumoliam23 <mumoliam23@gmail.com>
2023-12-01 03:32:56 +0300

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    if (len(sys.argv) - 1) == 0:
+        print(len(sys.argv) - 1, "arguments.")
+    elif (len(sys.argv) - 1) == 1:
+        print(len(sys.argv) - 1, "argument:")
+    else:
+        print(len(sys.argv) - 1, "arguments:")
+# Print the list of arguments with numbers
+    for i, arg in enumerate(sys.argv[1:]):
+        print("{}: {}".format(i + 1, arg))
