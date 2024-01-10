0d3c1b62fb0af268fb141ee45d47dbbbb0d93e95
mumoliam23 <mumoliam23@gmail.com>
2024-01-11 02:34:40 +0300

    1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,3 +5,19 @@ A function that writes a string to a text file and returns the numbers of charac
 def write_file(filename="", text=""):
     with open('filename', 'w') as file_object:
         return file_object.write(text)
+
+''' function that returns the number of lines of a text file
+'''
+
+
+def number_of_lines(filename=""):
+    ''' function: number_of_lines
+    '''
+    if filename == "" or type(filename) != str:
+        return 0
+    counter = 0
+    with open(filename, 'r') as f:
+        for line in f:
+            counter += 1
+    return counter
+
