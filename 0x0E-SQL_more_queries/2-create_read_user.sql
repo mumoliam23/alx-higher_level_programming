daf2921e21b42900d33d1c2680d334840c3e737b
mumoliam23 <mumoliam23@gmail.com>
2024-02-15 03:08:59 +0300

    A script that creates a database and a user and grants select privileges to use.

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,4 +4,4 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- create a user
 CREATE USER IF NOT EXITS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
 -- grants SELECT privileges to a user
-GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
+GRANT SELECT ON hbtn_0d_2. * TO user_0d_2@localhost;
