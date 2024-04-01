7c81ba579c23d0c4d6b2467ab25b208d82567228
mumoliam23 <mumoliam23@gmail.com>
2024-04-01 20:59:05 +0300

    1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send a GET request to an URL with curl, and display the body of the response
-curl -s -i "url" | grep HTTP/1.1\ 200
+url=$1; curl -s -w "%{http_code}" "url" | grep -q 200 && curl -s "url"
