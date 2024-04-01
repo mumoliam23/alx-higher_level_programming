473c9b75655a57847a3771d7c727a644ba2616b8
mumoliam23 <mumoliam23@gmail.com>
2024-04-02 00:30:45 +0300

    102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response
-curl -s -X POST -o /dev/null -w "%{http_code}" 0.0.0.0:5000/catch_me
+curl -s -X POST -o /dev/null -w "%{You got me!}" 0.0.0.0:5000/catch_me
