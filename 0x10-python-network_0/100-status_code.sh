b4b6de11b3bca0c9eede7b36c77977a50f34daa5
mumoliam23 <mumoliam23@gmail.com>
2024-04-02 00:14:00 +0300

    100-status_code.sh

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
-curl -sX POST $1 -H "Content-Type: application/json" -d @$2 -L
+#sends a request to a URL and displays the status code of the response
+url=$1; curl -s -w'%{response_code}' "$1"
