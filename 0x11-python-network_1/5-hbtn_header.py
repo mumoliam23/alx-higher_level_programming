3b5af3ff162460d4a0bbf49dc89c8090449b41c4
mumoliam23 <mumoliam23@gmail.com>
2024-04-02 04:04:02 +0300

    5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -8,13 +8,8 @@ import requests
 from sys import argv
 
 
-if __name__ == "__main__":
-    """takes in a URL, sends a request to the URL and displays
-    the value of the X-Request-Id variable found in the header
-    of the response using requests"""
-    r = requests.get(argv[1])
-    try:
-        r_id = r.headers['X-Request-Id']
-        print(r_id)
-    except:
-        pass
+if __name__ == '__main__':
+    url = argv[1]
+    r = requests.get(url)
+    r = r.headers
+    print(r.get('X-Request-Id'))
