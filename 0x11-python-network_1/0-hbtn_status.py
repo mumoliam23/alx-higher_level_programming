e521eb2f5e815dc08fcb77e44fa2cad9c98716f6
mumoliam23 <mumoliam23@gmail.com>
2024-04-02 03:51:21 +0300

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-""" Python script that fetches https://intranet.hbtn.io/status """
+"""fetches https://intranet.hbtn.io/status"""
+import urllib.request
 
 
-from urllib.request import urlopen
+if __name__ == "__main__":
+    """fetches https://intranet.hbtn.io/status"""
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        html_str = html.decode('utf-8')
 
-if __name__ == '__main__':
-    with urlopen('https://intranet.hbtn.io/status') as response:
-        data = response.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(data)))
-        print('\t- content: {}'.format(data))
-        print('\t- utf8 content: {}'.format(data.decode('utf-8')))
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(html_str))
