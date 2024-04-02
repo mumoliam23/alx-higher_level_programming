d887f98014476f18a0965d3de684467f9a6d17cd
mumoliam23 <mumoliam23@gmail.com>
2024-04-02 03:49:13 +0300

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-"""fetching a url using urllib"""
+""" Python script that fetches https://intranet.hbtn.io/status """
 
-import urllib.request
 
-url = "https://alx-intranet.hbtn.io/status"
-req = urllib.request.Request(url)
+from urllib.request import urlopen
 
-with urllib.request.urlopen(req) as res:
-    body = res.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(body)))
-    print("\t- content: {}".format(body))
-    print("\t- utf8 content: {}".format(body.decode("utf-8")))
+if __name__ == '__main__':
+    with urlopen('https://intranet.hbtn.io/status') as response:
+        data = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(data)))
+        print('\t- content: {}'.format(data))
+        print('\t- utf8 content: {}'.format(data.decode('utf-8')))
