aaa42b69dc1c6de204a642ee856ea61da65a25cd
mumoliam23 <mumoliam23@gmail.com>
2024-04-02 04:06:29 +0300

    7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,15 +6,9 @@ body of the response using requests
 import requests
 from sys import argv
 
-
-if __name__ == "__main__":
-    """
-    takes in a URL, sends a request to the URL and displays the
-    body of the response using requests
-    """
-    url = argv[1]
-    r = requests.get(url)
-    if r.status_code >= 400:
-        print("Error code: {}".format(r.status_code))
+if __name__ == '__main__':
+    response = requests.get(argv[1])
+    if response.status_code >= 400:
+        print("Error code: {}".format(response.status_code))
     else:
-        print(r.text)
+        print(response.text)
