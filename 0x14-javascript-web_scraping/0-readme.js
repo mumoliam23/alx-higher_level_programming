511194c4547fb075bd206c3e2a60872bd8c89e1c
mumoliam23 <mumoliam23@gmail.com>
2024-04-25 00:16:15 +0300

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,14 +1,17 @@
 #!/usr/bin/node
+
 let fs = require('fs');
 let file = process.argv[2];
 
-function readPrint (file) {
-  fs.readFile(file, 'utf-8', function (err, data) {
+function readPrint(file) {
+  fs.readFile(file, 'utf-8', function(err, data) {
     if (err) {
-      console.log(err);
+      console.log("Error occurred while reading the file:", err);
     } else {
       console.log(data);
     }
   });
 }
+
 readPrint(file);
+
