a3d56feacf99ba5e3db9862864053ab90493a167
mumoliam23 <mumoliam23@gmail.com>
2024-04-29 22:03:53 +0300

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,10 +2,16 @@
 
 const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf8', function (err, data) {
-  if (err) {
-    console.log(err);
-  } else {
-    process.stdout.write(data);
-  }
+const filePath = process.argv[2];
+
+if (!filePath) {
+	console.error('Please provide a file path as an argument.');
+	process.exit(1);
+}
+fs.readFile(filePath, 'utf-8', (err, data) => {
+	if (err) {
+		console.error(err);
+	}else {
+		process.stdout.write(data);
+	}
 });
