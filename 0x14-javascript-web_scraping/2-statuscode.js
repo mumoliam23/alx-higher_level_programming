7041f496c0edaebf4c658af435e4921856a1bff2
mumoliam23 <mumoliam23@gmail.com>
2024-04-30 19:10:55 +0300

    2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,16 +5,16 @@ const request = require('request');
 const url = process.argv[2];
 
 if (!url) {
-	console.error('Error: ', error.message);
-	process.exit(1);
+  console.error('Error: ', error.message);
+  process.exit(1);
 }
 request(url, (error, response, body) => {
-	if (error) {
-		console.error('Error: ', error.message);
-		process.exit(1);
+  if (error) {
+    console.error('Error: ', error.message);
+    process.exit(1);
 	} else {
 		if (response && response.statusCode) {
-			console.log('Code: ', response.statusCode);
+		  console.log('code: ', response.statusCode);
 		} else {
 			console.error('Error: ', error.message);
 			process.exit(1);
