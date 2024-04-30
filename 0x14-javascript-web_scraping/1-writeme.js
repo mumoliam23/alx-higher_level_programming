acdde8c44c08bfd62e6e76aa00752492cbcd8e84
mumoliam23 <mumoliam23@gmail.com>
2024-04-30 03:05:26 +0300

    1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,16 +2,8 @@
 
 const fs = require('fs');
 
-const filePath = process.argv[2];
-const content = process.argv[3];
-if (!filePath || !content) {
-	console.error('Usage: node 1-writeme.js <file_path> "<content>"');
-	process.exit(1);
-}
-fs.writeFile(filePath, content, 'utf-8', (err) => {
-	if (err) {
-		console.error(err);
-	} else {
-		console.log('Sucessfully wrote content to ${filePath}');
-	}
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (err) {
+  if (err) {
+    console.log(err);
+  }
 });
